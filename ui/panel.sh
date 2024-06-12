b1b7521bd7c922936741a394ecbee4c9e507b449
Çağlar <caglar.yml@gmail.com>
2024-06-12 18:03:38 +0300

    Update panel.sh

--- ui/panel.sh
+++ ui/panel.sh
@@ -33,7 +33,7 @@ fn_exists() { declare -F "$1" >/dev/null; }
 if ! fn_exists lib_loaded; then
   # shellcheck source=lib/lib.sh
   source /tmp/lib.sh || source <(curl -sSL "$GITHUB_BASE_URL/$GITHUB_SOURCE"/lib/lib.sh)
-  ! fn_exists lib_loaded && echo "* ERROR: Could not load lib script" && exit 1
+  ! fn_exists lib_loaded && echo "* HATA: Lib betigi yuklenemedi" && exit 1
 fi
 
 # ------------------ Variables ----------------- #
@@ -68,10 +68,10 @@ export CONFIGURE_FIREWALL=false
 
 ask_letsencrypt() {
   if [ "$CONFIGURE_UFW" == false ] && [ "$CONFIGURE_FIREWALL_CMD" == false ]; then
-    warning "Let's Encrypt requires port 80/443 to be opened! You have opted out of the automatic firewall configuration; use this at your own risk (if port 80/443 is closed, the script will fail)!"
+    warning "Let's Encrypt 80/443 numarali baglanti noktasinin acilmasini gerektirir! Otomatik guvenlik duvari yapilandirmasini devre disi biraktiniz; bunu kendi sorumlulugunuzda kullanin (80/443 numarali baglanti noktasi kapaliysa, betik basarisiz olur)!"
   fi
 
-  echo -e -n "* Do you want to automatically configure HTTPS using Let's Encrypt? (y/N): "
+  echo -e -n "* Let's Encrypt kullanarak HTTPS'yi otomatik olarak yapilandirmak istiyor musunuz? (y/N): "
   read -r CONFIRM_SSL
 
   if [[ "$CONFIRM_SSL" =~ [Yy] ]]; then
@@ -81,10 +81,10 @@ ask_letsencrypt() {
 }
 
 ask_assume_ssl() {
-  output "Let's Encrypt is not going to be automatically configured by this script (user opted out)."
-  output "You can 'assume' Let's Encrypt, which means the script will download a nginx configuration that is configured to use a Let's Encrypt certificate but the script won't obtain the certificate for you."
-  output "If you assume SSL and do not obtain the certificate, your installation will not work."
-  echo -n "* Assume SSL or not? (y/N): "
+  output "Let's Encrypt bu kod tarafindan otomatik olarak yapilandirilmiyacaj (kullanici devre disi birakti)."
+  output "Let's Encrypt'i 'varsayabilirsiniz', bu da betigin Let's Encrypt sertifikasi kullanmak uzere yapilandirilmis bir nginx yapilandirmasi indirecegi, ancak betigin sertifikayi sizin icin almayacagi anlamina gelir."
+  output "SSL varsayarsaniz ve sertifikayi almazsaniz, kuruluminuz calismayacaktir."
+  echo -n "* SSL varsayalim mi, varsaymayalim mi? (y/N):"
   read -r ASSUME_SSL_INPUT
 
   [[ "$ASSUME_SSL_INPUT" =~ [Yy] ]] && ASSUME_SSL=true
@@ -95,19 +95,19 @@ check_FQDN_SSL() {
   if [[ $(invalid_ip "$FQDN") == 1 && $FQDN != 'localhost' ]]; then
     SSL_AVAILABLE=true
   else
-    warning "* Let's Encrypt will not be available for IP addresses."
-    output "To use Let's Encrypt, you must use a valid domain name."
+    warning "* Let's Encrypt IP adresleri icin kullanilamayacak."
+    output "Let's Encrypt'i kullanmak icin gecerli bir alan adi kullanmaniz gerekir."
   fi
 }
 
 main() {
   # check if we can detect an already existing installation
   if [ -d "/var/www/pterodactyl" ]; then
-    warning "The script has detected that you already have Pterodactyl panel on your system! You cannot run the script multiple times, it will fail!"
-    echo -e -n "* Are you sure you want to proceed? (y/N): "
+    warning "Betik, sisteminizde zaten Pterodactyl paneli oldugunu tespit etti! Betigi birden fazla kez calistiramazsiniz, basarisiz olur!"
+    echo -e -n "* Devam etmek istiyor musun? (y/N): "
     read -r CONFIRM_PROCEED
     if [[ ! "$CONFIRM_PROCEED" =~ [Yy] ]]; then
-      error "Installation aborted!"
+      error "Kurulum iptal edildi!"
       exit 1
     fi
   fi
@@ -117,56 +117,56 @@ main() {
   check_os_x86_64
 
   # set database credentials
-  output "Database configuration."
-  output ""
-  output "This will be the credentials used for communication between the MySQL"
-  output "database and the panel. You do not need to create the database"
-  output "before running this script, the script will do that for you."
-  output ""
+output "Veritabani yapilandirmasi."
+output ""
+output "Bu, MySQL arasindaki iletisim icin kullanilan kimlik bilgileri olacaktir"
+output "veritabani ve panel. Veritabani olusturmaniza gerek yok"
+output "bu betigi calistirmadan once, betik bunu sizin icin yapacaktir."
+output ""
 
   MYSQL_DB="-"
   while [[ "$MYSQL_DB" == *"-"* ]]; do
-    required_input MYSQL_DB "Database name (panel): " "" "panel"
-    [[ "$MYSQL_DB" == *"-"* ]] && error "Database name cannot contain hyphens"
+    required_input MYSQL_DB "Veritabani adi (panel): " "" "panel"
+    [[ "$MYSQL_DB" == *"-"* ]] && error "Veritabani adi kisa cizgi iceremez"
   done
 
   MYSQL_USER="-"
   while [[ "$MYSQL_USER" == *"-"* ]]; do
-    required_input MYSQL_USER "Database username (pterodactyl): " "" "pterodactyl"
-    [[ "$MYSQL_USER" == *"-"* ]] && error "Database user cannot contain hyphens"
+    required_input MYSQL_USER "Veritabani kullanici adi (pterodactyl): " "" "pterodactyl"
+    [[ "$MYSQL_USER" == *"-"* ]] && error "Veritabani kullanici adi kisa cizgi iceremez"
   done
 
   # MySQL password input
   rand_pw=$(gen_passwd 64)
-  password_input MYSQL_PASSWORD "Password (press enter to use randomly generated password): " "MySQL password cannot be empty" "$rand_pw"
+  password_input MYSQL_PASSWORD "Veritabani sifresi (rastgele sifre olusturmak icin enter tusuna basin): " "MySQL parolasi bos olamaz" "$rand_pw"
 
   readarray -t valid_timezones <<<"$(curl -s "$GITHUB_URL"/configs/valid_timezones.txt)"
-  output "List of valid timezones here $(hyperlink "https://www.php.net/manual/en/timezones.php")"
+  output "Gecerli saat dilimlerinin listesi burada $(hyperlink "https://www.php.net/manual/en/timezones.php")"
 
   while [ -z "$timezone" ]; do
-    echo -n "* Select timezone [Europe/Stockholm]: "
+    echo -n "* Zaman dilimi sec [Europe/Istanbul]: "
     read -r timezone_input
 
     array_contains_element "$timezone_input" "${valid_timezones[@]}" && timezone="$timezone_input"
-    [ -z "$timezone_input" ] && timezone="Europe/Stockholm" # because köttbullar!
+    [ -z "$timezone_input" ] && timezone="Europe/Istanbul" # because köttbullar!
   done
 
-  email_input email "Provide the email address that will be used to configure Let's Encrypt and Pterodactyl: " "Email cannot be empty or invalid"
+  email_input email "Let's Encrypt ve Pterodactyl'i yapilandirmak icin kullanilacak e-posta adresini girin: " "E-posta bos veya gecersiz olamaz"
 
   # Initial admin account
-  email_input user_email "Email address for the initial admin account: " "Email cannot be empty or invalid"
-  required_input user_username "Username for the initial admin account: " "Username cannot be empty"
-  required_input user_firstname "First name for the initial admin account: " "Name cannot be empty"
-  required_input user_lastname "Last name for the initial admin account: " "Name cannot be empty"
-  password_input user_password "Password for the initial admin account: " "Password cannot be empty"
+  email_input user_email "Ilk yonetici hesabi icin e-posta adresi: " "Email bos olamaz veya gecersizdir"
+  required_input user_username "Ilk yonetici hesabi icin kullanici adi: " "Username bos olamaz"
+  required_input user_firstname "Ilk yonetici hesabi icin ilk reklam: " "Isim bos olamaz"
+  required_input user_lastname "Ilk yonetici hesabi icin soyadi: " "Ad bos olamaz"
+  password_input user_password "Ilk yonetici hesabi icin parola: " "Parola bos olamaz"
 
   print_brake 72
 
   # set FQDN
   while [ -z "$FQDN" ]; do
-    echo -n "* Set the FQDN of this panel (panel.example.com): "
+    echo -n "* Bu panelin FQDN'sini ayarlayin (panel.example.com): "
     read -r FQDN
-    [ -z "$FQDN" ] && error "FQDN cannot be empty"
+    [ -z "$FQDN" ] && error "FQDN bos olamaz."
   done
 
   # Check if SSL is available
@@ -190,49 +190,49 @@ main() {
   summary
 
   # confirm installation
-  echo -e -n "\n* Initial configuration completed. Continue with installation? (y/N): "
+  echo -e -n "\n* Ilk yapilandirma tamamlandi. Kuruluma devam edelim mi? (y/N): "
   read -r CONFIRM
   if [[ "$CONFIRM" =~ [Yy] ]]; then
     run_installer "panel"
   else
-    error "Installation aborted."
+    error "Kurulum iptal edildi."
     exit 1
   fi
 }
 
 summary() {
   print_brake 62
-  output "Pterodactyl panel $PTERODACTYL_PANEL_VERSION with nginx on $OS"
-  output "Database name: $MYSQL_DB"
-  output "Database user: $MYSQL_USER"
-  output "Database password: (censored)"
-  output "Timezone: $timezone"
-  output "Email: $email"
-  output "User email: $user_email"
-  output "Username: $user_username"
-  output "First name: $user_firstname"
-  output "Last name: $user_lastname"
-  output "User password: (censored)"
-  output "Hostname/FQDN: $FQDN"
-  output "Configure Firewall? $CONFIGURE_FIREWALL"
-  output "Configure Let's Encrypt? $CONFIGURE_LETSENCRYPT"
-  output "Assume SSL? $ASSUME_SSL"
+  output "$OS uzerinde Nginx ile Pterodactyl $PTERODACTYL_PANEL_VERSION $OS"
+  output "Veritabani adi: $MYSQL_DB"
+  output "Veritabani kullanicisi: $MYSQL_USER"
+  output "Veritabani parolasi: (sansurlu)"
+  output "Zaman dilimi: $timezone"
+  output "E-posta: $email"
+  output "Kullanici e-postasi: $user_email"
+  output "Kullanici adi: $user_username"
+  output "Ilk ad: $user_firstname"
+  output "Soyadi: $user_lastname"
+  output "Kullanici parolasi: (sansurlu)"
+  output "FQDN: $FQDN"
+  output "Guvenlik Duvari Yapilandirilsin mi? $CONFIGURE_FIREWALL"
+  output "Let's Encrypt'i Yapilandir? $CONFIGURE_LETSENCRYPT"
+  output "SSL varsayalim mi? $ASSUME_SSL"
   print_brake 62
 }
 
 goodbye() {
   print_brake 62
-  output "Panel installation completed"
+  output "Panel kurulumu tamamlandi"
   output ""
 
-  [ "$CONFIGURE_LETSENCRYPT" == true ] && output "Your panel should be accessible from $(hyperlink "$FQDN")"
-  [ "$ASSUME_SSL" == true ] && [ "$CONFIGURE_LETSENCRYPT" == false ] && output "You have opted in to use SSL, but not via Let's Encrypt automatically. Your panel will not work until SSL has been configured."
-  [ "$ASSUME_SSL" == false ] && [ "$CONFIGURE_LETSENCRYPT" == false ] && output "Your panel should be accessible from $(hyperlink "$FQDN")"
+  [ "$CONFIGURE_LETSENCRYPT" == true ] && output "Panelinize $(hyperlink "$FQDN") adresinden erisilebilmelidir."
+  [ "$ASSUME_SSL" == true ] && [ "$CONFIGURE_LETSENCRYPT" == false ] && output "SSL kullanmayi sectiniz, ancak Let's Encrypt araciligiyla otomatik olarak degil. SSL yapilandirilana kadar paneliniz calismayacaktir."
+  [ "$ASSUME_SSL" == false ] && [ "$CONFIGURE_LETSENCRYPT" == false ] && output "Panelinize $(hyperlink "$FQDN") adresinden erisilebilmelidir."
 
   output ""
-  output "Installation is using nginx on $OS"
-  output "Thank you for using this script."
-  [ "$CONFIGURE_FIREWALL" == false ] && echo -e "* ${COLOR_RED}Note${COLOR_NC}: If you haven't configured the firewall: 80/443 (HTTP/HTTPS) is required to be open!"
+  output "Kurulum $OS üzerinde nginx kullanıyor"
+  output "Bu betigi kullandiginiz icin tesekkur ederiz."
+  [ "$CONFIGURE_FIREWALL" == false ] && echo -e "* ${COLOR_RED}Note${COLOR_NC}:Eger guvenlik duvarini yapilandirmadiysaniz: 80/443 (HTTP/HTTPS) acik olmalidir!"
   print_brake 62
 }
 
