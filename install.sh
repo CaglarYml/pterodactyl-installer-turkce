106afd27b3cd2618179e111c523fb4a3b145914a
Çağlar <caglar.yml@gmail.com>
2024-06-12 12:41:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,14 +34,14 @@ export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
-# check for curl
+# curl için kontrol edin
 if ! [ -x "$(command -v curl)" ]; then
-  echo "* curl is required in order for this script to work."
-  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  echo "* Bu betiğin çalışması için curl gereklidir."
+  echo "* apt (Debian ve türevleri) veya yum/dnf (CentOS) kullanarak yükleyin"
   exit 1
 fi
 
-# Always remove lib.sh, before downloading it
+# İndirmeden önce her zaman lib.sh dosyasını kaldırın
 rm -rf /tmp/lib.sh
 curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/"$GITHUB_SOURCE"/lib/lib.sh
 # shellcheck source=lib/lib.sh
@@ -55,7 +55,7 @@ execute() {
   run_ui "${1//_canary/}" |& tee -a $LOG_PATH
 
   if [[ -n $2 ]]; then
-    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
+    echo -e -n "* Kurulum $1 tamamlandi. $2 kurulumuna devam etmek istiyor musunuz? (y/N): "
     read -r CONFIRM
     if [[ "$CONFIRM" =~ [Yy] ]]; then
       execute "$2"
@@ -71,15 +71,15 @@ welcome ""
 done=false
 while [ "$done" == false ]; do
   options=(
-    "Install the panel"
-    "Install Wings"
-    "Install both [0] and [1] on the same machine (wings script runs after panel)"
+    "Panel Kur"
+    "Wings Kur"
+    "Hem [0] hem de [1]i beraber kurun. (Wings betigi panelden sonra calisir.)"
     # "Uninstall panel or wings\n"
 
-    "Install panel with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install Wings with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install both [3] and [4] on the same machine (wings script runs after panel)"
-    "Uninstall panel or wings with canary version of the script (the versions that lives in master, may be broken!)"
+    "Paneli betigin test surumu ile yukleyin (master'da bulunan surumler bozuk olabilir!)"
+    "Wingsi betigin test surumu ile yukleyin (master'da bulunan surumler bozuk olabilir!)"
+    "Hem [3] hem de [4]u beraber kurun. (Wings betigi panelden sonra calisir.)"
+    "Betigin test surumuyle Panel ve Wingsi kaldırın. (master'da bulunan surumler bozuk olabilir!)"
   )
 
   actions=(
@@ -94,19 +94,19 @@ while [ "$done" == false ]; do
     "uninstall_canary"
   )
 
-  output "What would you like to do?"
+  output "Ne yapmak istersiniz?"
 
   for i in "${!options[@]}"; do
     output "[$i] ${options[$i]}"
   done
 
-  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  echo -n "* Girdi 0-$((${#actions[@]} - 1)): "
   read -r action
 
-  [ -z "$action" ] && error "Input is required" && continue
+  [ -z "$action" ] && error "Girdi gereklidir" && continue
 
   valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
-  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Gecersiz secenek"
   [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
 done
 
